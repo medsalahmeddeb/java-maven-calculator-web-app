7cd99147e7293af326e20008f2f600c84b660463
Mohamed Salah MEDDEB <medsalah.meddeb@gmail.com>
2021-01-21 14:26:25 +0100

    first commit

---
+++ src/test/java/com/qianhong/calculator/CalculatorServiceTest.java
@@ -0,0 +1,11 @@
+package com.qianhong.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
+
+public class CalculatorServiceTest {
+
+}
